e86fdde539a6f790d9bca7bc8d3a6275eed5b973
Susanmuthoni <s.njeri@alustudent.com>
2026-09-13 20:59:48 +0400

    Added room display method

--- models/room.py
+++ models/room.py
@@ -5,12 +5,24 @@ class Room:
         self.price = price
         self.available = True
 
+    def display_room(self):
+        status = "Available" if self.available else "Occupied"
+
+        print(f"Room Number: {self.room_number}")
+        print(f"Room Type: {self.room_type}")
+        print(f"Price per Night: ${self.price:.2f}")
+        print(f"Status: {status}")
+
+room1 = Room("101", "Single", 50)
+room1.display_room()
+
+
 room1 = Room("101", "Single", 50)
 
 print(room1.room_number)
 print(room1.room_type)
 print(room1.price)
 print(room1.available)
+room1.display_room()
 
     
-   
